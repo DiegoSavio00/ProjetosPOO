7dc34563ec2c9723131ae37da6251dede550a08d
diego <diego@DESKTOP-VBD0RNK>
2024-01-04 14:52:18 -0300

    Add AlunosPOO

---
+++ AlunosPOO/Aluno.cs
@@ -0,0 +1,36 @@
+﻿namespace AlunosPOO
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        private double[] _notas;
+
+        public double Media { get => CalcularMedia(); }
+
+        public Aluno(string nome, int provas)
+        {
+            Nome = nome;
+            _notas = new double[provas];
+        }
+
+        public void InserirNotas()
+        {
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                Console.WriteLine("Nota #" + (i + 1) + " : ");
+                _notas[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        private double CalcularMedia()
+        {
+            double total = 0;
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                total += _notas[i];
+            }
+            return total / _notas.Length;
+        }
+
+    }
+}
